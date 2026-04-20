7cddcb096c219cbcaa0a5c243b88465e8e2c83e2
chinmoy10 <chinmoypodder22@gmail.com>
2026-04-21 02:23:55 +0600

    running commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar"
+import NavbarCopy from "./components/NavbarCopy"
 
 function App() {
 
   return (
     <>
       <Navbar></Navbar>
+      <NavbarCopy></NavbarCopy>
       <h1 className='text-teal-500 font-extrabold text-3xl'>Get started our mission</h1>
     </>
   )
