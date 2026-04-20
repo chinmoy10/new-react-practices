42833d794c19c99bffd59f59cb073a52e404209a
chinmoy10 <chinmoypodder22@gmail.com>
2026-04-21 01:16:04 +0600

    start commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <h1 className='text-teal-500 font-extrabold text-3xl'>Get started our mission</h1>
+    </>
+  )
+}
+
+export default App
